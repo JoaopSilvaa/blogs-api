c17d7651b605d5078fb4a9acf34d02f8bc2014b2
Joao Antonio <joaopsilva.dev@gmail.com>
2022-07-15 15:25:43 -0300

    fix: :bug: correção do requisito do 14

--- src/services/getPostById.js
+++ src/services/getPostById.js
@@ -1,16 +1,12 @@
-const { PostCategory, BlogPost, Category, User } = require('../database/models');
+const { BlogPost, Category, User } = require('../database/models');
 
 const getPostById = async (id) => {
-    const post = await PostCategory.findOne({ 
-        where: { postId: id },
+    const post = await BlogPost.findOne({ 
+        where: { id },
         include: [
-            { model: BlogPost, 
-                as: 'blogPost',
-                through: { attributes: [],
-                include: [{ model: User, as: 'user', attributes: { exclude: 'password' } }] } },
+            { model: User, as: 'user', attributes: { exclude: 'password' } },
             { model: Category, as: 'categories', through: { attributes: [] } },
-        ],
-    });
+        ] });
     if (!post) {
         return {
             error: { code: 'notFound', message: 'Post does not exist' },
